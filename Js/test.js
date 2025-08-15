092c3ab3c7bd33d0e9c6be97d9808fb953743a04
Thulana Dulan Jeewantha Amaranath <128315509+TwIsTeDDuLan@users.noreply.github.com>
2025-08-15 23:28:01 +0530

    learning java

---
+++ Js/test.js
@@ -0,0 +1,12 @@
+function changeText() {
+    alert('Fuck You Bitch!');
+}
+
+function changeColor(x){
+    if(x == 1){
+        document.getElementById("b").style="background-clor='blue'";
+    }
+    else{
+        document.getElementById("b").style="background-clor='red'";
+    }
+}
